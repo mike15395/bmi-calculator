a9a309ee237ae755c166d6c7458071aa58272216
mike15395 <mikhildesai@gmail.com>
2025-03-22 00:10:25 +0530

    final-code-done

--- starter-code/script.js
+++ starter-code/script.js
@@ -14,6 +14,8 @@ let weightPound = document.getElementById("weight-pound");
 
 const bmiResultSection = document.getElementById("result-section");
 const bmiScore = document.getElementById("score");
+const bmiClassification = document.getElementById("classification");
+const bmiRange = document.getElementById("range");
 
 let [...radios] = document.querySelectorAll('input[type="radio"]');
 radios.map((ele) => {
@@ -30,21 +32,64 @@ radios.map((ele) => {
 
 bmiResultSection.addEventListener("click", function () {
   if (metricInput.checked) {
+    if (!metricHeight.value || !metricWeight.value) {
+      alert("please enter correct values");
+      return;
+    }
+
     let heightInMetres = metricHeight.value * 0.01;
     let calcMetric = metricWeight.value / (heightInMetres * heightInMetres);
     let bmiMetric = Math.floor(calcMetric * 100) / 100;
-
-    console.log(bmiMetric, "bmi metric");
     bmiScore.innerHTML = bmiMetric;
+    bmiClassification.innerHTML = giveBmiClassification(bmiMetric);
+    bmiRange.innerHTML = healthyRangeMetric(heightInMetres);
   } else {
+    if (
+      !heightFeet.value ||
+      !heightInch.value ||
+      !weightPound.value ||
+      !weightStone.value
+    ) {
+      alert("Please enter correct values");
+      return;
+    }
+
     let totalHeightInch =
       Number(heightFeet.value * 12) + Number(heightInch.value);
-    console.log(totalHeightInch, "total inch height");
     let totalWeightPound =
       Number(weightStone.value * 14) + Number(weightPound.value);
-    console.log(totalWeightPound, "total weight pound");
     let calcImperial = totalWeightPound / (totalHeightInch * totalHeightInch);
     let bmiImperial = Math.floor(calcImperial * 703);
     bmiScore.innerHTML = bmiImperial;
+    bmiClassification.innerHTML = giveBmiClassification(bmiImperial);
+    bmiRange.innerHTML = healthyRangeImperial(totalHeightInch);
   }
 });
+
+function giveBmiClassification(bmi) {
+  if (bmi <= 18.5) {
+    return "Underweight";
+  } else if (bmi > 18.5 && bmi <= 24.9) {
+    return "Healthy";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    return "Overweight";
+  } else if (bmi >= 30) {
+    return "Obese";
+  }
+}
+
+function healthyRangeMetric(height) {
+  let minWeight = Math.floor(18.5 * height * height * 100) / 100;
+  let maxWeight = Math.floor(24.9 * height * height * 100) / 100;
+
+  return `${minWeight}kgs-${maxWeight}kgs`;
+}
+
+function healthyRangeImperial(height) {
+  let minWeight = (18.5 * height * height) / 703;
+  let maxWeight = (24.9 * height * height) / 703;
+
+  return `${Math.floor(minWeight * 100) / 100}lbs-${
+    Math.floor(maxWeight * 100) / 100
+  }lbs`;
+}
